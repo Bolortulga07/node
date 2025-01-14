0288ccb635b6a0b34a8b8ef6080a1a99c270dbfa
bolortulga10 <bolortulga07@gmail.com>
2025-01-14 20:08:46 +0800

    zassan

--- src/app.js
+++ src/app.js
@@ -1,45 +1,29 @@
 import express from "express";
 import fs from "fs";
-import { route } from "./modules/movies/routes/moviesRoutes.js";
+import { route as movieRoutes } from "./modules/movies/routes/moviesRoutes.js";
 
 const app = express();
-const port = 3000;
+
 app.use(express.json());
 
+app.use("/movies", movieRoutes);
+
 app.get("/", (req, res) => {
+  //html info page
   const html = fs.readFileSync("./index.html");
 
   res.setHeader("Content-type", "text/html");
-
   res.send(html);
-});
-
-app.get("/movies", (req, res) => {
-  const movies = JSON.parse(fs.readFileSync("./movies.json"));
-
-  const { movieTitle } = req.query;
-  console.log(movieTitle);
-
-  let result;
-
-  if (movieTitle) {
-    result = movies.filter((movie) =>
-      movie.title.toLowerCase().includes(movieTitle.toLowerCase())
-    );
-  } else {
-    result = movies;
-  }
-  res.json(result);
 });
 
 app.post("/movies", (req, res) => {
   const movie = req.body;
-  console.log(movie);
+
   const movies = JSON.parse(fs.readFileSync("./movies.json"));
 
   movies.push(movie);
 
-  fs.writeFileSync("./movies.json", JSON.stringify(movies));
+  fs.writeFileSync("./movies.json", movies);
 
   res.send({ success: true, message: "movie added" });
 });
